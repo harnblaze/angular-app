455551dba82ba6a837c1ccddc7633aa2aab5964b
Dmitriy Emelyanov <harnblaze@gmail.com>
2022-08-24 23:40:02 +0300

    add loading components

---
+++ src/app/services/products.services.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams,
+} from '@angular/common/http';
+import { catchError, Observable, retry, throwError } from 'rxjs';
+import { IProduct } from '../models/product';
+import { ErrorService } from './error.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductsServices {
+  constructor(private http: HttpClient, private errorService: ErrorService) {}
+
+  getAll(): Observable<IProduct[]> {
+    return this.http
+      .get<IProduct[]>('https://fakestoreapi.com/products', {
+        params: new HttpParams({ fromObject: { limit: 5 } }),
+      })
+      .pipe(retry(2), catchError(this.errorHandler.bind(this)));
+  }
+
+  private errorHandler(error: HttpErrorResponse) {
+    this.errorService.handle(error.message);
+    return throwError(() => error.message);
+  }
+}
